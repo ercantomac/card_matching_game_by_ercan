7f313a49694268dd88aae78866cc8a397db5dfa4
Ercan Tomac <48109156+ercantomac@users.noreply.github.com>
2022-01-01 19:51:51 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  //Paint.enableDithering = true;
   runApp(const MyApp());
 }
 
